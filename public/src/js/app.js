8729ef2ecde98f3523dd1a46ba5c964c2eeac311
Nikki Petersen <piknik@me.com>
2019-06-12 11:26:32 -0600

    complete as can be for now

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,10 @@
 // should also use polyfills so that fetch can be used on any browser
 
-var deferredPrompt;
+var deferredPrompt; // this is for the prompt to add app to device
+
+if (!window.Promise) {
+    window.Promise = Promise;
+}
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,53 @@
-self.addEventListener('install', function(event) {
-    // console.log('[service worker] Installing service worker . . . ', event);
+var CACHE_STATIC_NAME = 'static-v5';
+var CACHE_DYNAMIC_NAME = 'dynamic-v5';
 
-    // open the cache
+
+self.addEventListener('install', function(event) {
+    console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
-        caches.open('static')
-            .then((cache) => {
-              // console.log('[service worker] precaching app shell');
+        caches.open(CACHE_STATIC_NAME)
+            .then(function(cache) {
+                console.log('[Service Worker] Precaching App Shell');
+
                 cache.addAll([
                     '/',
                     '/index.html',
                     '/src/js/app.js',
-                    '/src/css/style2.css'
-                ])
-            }))
+                    '/src/css/style2.css',
+                    '/src/images/burgers.png',
+                    '/src/images/clickgame.png',
+                    '/src/images/crystal.png',
+                    '/src/images/curve-doubleLeft.png',
+                    '/src/images/curve-singleRightLight.png',
+                    '/src/images/eventsearch.png',
+                    '/src/images/nikkinnorway.png',
+                    '/src/images/sociallyawkward.png',
+                    '/src/images/trains.png',
+                    'https://fonts.googleapis.com/css?family=Open+Sans|Patrick+Hand&display=swap',
+                    "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+                ]);
+            })
+    )
 });
 
 self.addEventListener('activate', function(event) {
-    // console.log('[service worker] Activating service worker . . . ', event);
+    console.log('[service worker] Activating service worker . . . ', event);
+    console.log('works up to this point');
+    event.waitUntil(
+        caches.keys()
+            .then(function(keyList) {
+                return Promise.all(keyList.map(function(key) {
+                    if (key !== CACHE_STATIC_NAME) {
+                        console.log("[service worker] removing old cache.", key);
+                        return caches.delete(key);
+                    }
+                }));
+            })
+    )
     return self.clients.claim();
 });
+
+
 
 // fetch works as a network proxy, assures that it always has data
 self.addEventListener('fetch', function(event) {
@@ -29,7 +58,17 @@ self.addEventListener('fetch', function(event) {
                 if (response) {
                     return response; // return value from cache
                 } else {
-                    return fetch(event.request); // if not in cache, get it.
+                    return fetch(event.request) // if not in cache, get it.
+                        .then(function(res) {
+                            return caches.open(CACHE_DYNAMIC_NAME)
+                                .then(function(cache) {
+                                    cache.put(event.request.url, res.clone()); // use clone bc it's used up on response
+                                    return res;
+                                })
+                        })
+                        .catch(function(err) {
+
+                        });
                 }
             })
     );
